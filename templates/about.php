0c14e8ee7983c29671abe114aa57f72632ef5b3f
Thomas <latiosthinh@live.com>
2020-05-30 10:21:07 +0700

    about

--- templates/about.php
+++ templates/about.php
@@ -5,19 +5,40 @@
 get_header();
 ?>
 
-<div class="container" style="margin:60px auto">
-    <div class="row" style="justify-content: center;">
-        <div class="col-md-10">
-            <img src="<?php echo IMG . '/about.png' ?>">
+<section class="about">
+    <div class="container">
+        <div class="row vision">
+            <div class="col-md-4">
+                <p class="grey">Our Visions and Values</p>
+            </div>
+
+            <div class="col-md-8">
+                <p><?= rwmb_meta( 'vision' ) ?></p>
+            </div>
+        </div>
+
+        <div class="row services">
+            <div class="col-md-12">
+                <h1>High-quality architectural services</h1>
+            </div>
+            <?php
+            $services = rwmb_meta( 'services' );
+            if ( ! empty( $services ) ) :
+                foreach ( $services as $s ) :
+            ?>
+                <div class="col-md-4 item">
+                    <h3><?= $s['title']  ?></h3>
+                    <p><?= $s['detail'] ?></p>
+                </div>
+            <?php
+                endforeach;
+            endif;
+            ?>
         </div>
     </div>
-</div>
 
-<div class="container-fluid" style="margin-bottom:60px">
-    <div class="row">
-        <img src="<?php echo IMG . '/future-product.png' ?>" style="width:100%">
-    </div>
-</div>
+    <img class="about-img" src="<?= IMG . '/banner-1.jpg' ?>">
+</section>
 
 <?php
 get_footer();
