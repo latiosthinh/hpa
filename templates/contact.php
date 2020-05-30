8f7bb3da20a1549b687e748235f1d285b4457a54
Thomas <latiosthinh@live.com>
2020-05-30 09:25:46 +0700

    contacts

--- templates/contact.php
+++ templates/contact.php
@@ -7,22 +7,42 @@ get_header();
 ?>
 
 <section class="contact">
-    <h2>Contact us</h2>
-
     <div class="container">
-        <div class="row">
+        <h2>Contact us</h2>
+        
+        <div class="row contact-info">
+            <div class="col-md-6">
+                <p class="grey">
+                Feel free to email or call us to talk about your requirements <br>and find out more about what we can do for you.
+                </p>
+            </div>
 
-            <div class="col-md-1"></div>
-            <div class="col-md-5 social">
-                <div>
-                    <h3>You Can Submit A Contact Form Here <br>
-                        OR Reach Us Via:</h3>
+            <div class="col-md-3">
+                <p>
+                    1256 Hanoi, 2nd floor,<br> Vincom Mega Mall Tower
+                </p>
+            </div>
 
-                    <a href="#"><img src="<?php echo IMG . '/discord-logo.png' ?>">Discord Channel</a>
-                    <a href="#"><img src="<?php echo IMG . '/instagram-logo.png' ?>">Instagram</a>
-                    <a href="#"><img src="<?php echo IMG . '/facebook-logo.png' ?>">Our Facebook Fanpage</a>
-                    <a href="#"><img src="<?php echo IMG . '/youtube-logo.png' ?>">Youtube Channel</a>
-                </div>
+            <div class="col-md-3 d-flex flex-column">
+                <a href="mailto:support@hpa.com.vn">E. support@hpa.com.vn</a>
+                <a href="tel:+84 0826101991">T. +84 0826101991</a>
+            </div>
+        </div>
+
+        <div class="row map">
+            <?php
+            $icon = IMG . '/pin.png';
+            $args = array(
+                // 'width'        => '640px',
+                'height'       => '500px',
+                'zoom'         => 14,
+                'marker'       => true,
+                // 'marker_icon'  => $icon,
+            );
+            ?>
+
+            <div class="col-md-12">
+                <?php echo rwmb_meta( 'map', $args ); ?>
             </div>
         </div>
     </div>
