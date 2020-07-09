4adec6002365be312b5c2a650b8b6d6d0d188c18
Latios Thinh <latiosthinh@live.com>
2020-07-09 16:43:24 +0700

    fix about ( client's idea )

--- js/script.js
+++ js/script.js
@@ -86,39 +86,6 @@
 				touchThreshold: 100
 			})
 		}
-
-		if ( $( '.awards-timeline__slider' ).length !== 0 ) {
-			$('.awards-timeline__slider').slick({
-				// autoplay: true,
-				// autoplaySpeed: 3000,
-				draggable: true,
-				arrows: false,
-				// prevArrow: '<span class="prev"><ion-icon name="arrow-back"></ion-icon></span>',
-				// nextArrow: '<span class="next"><ion-icon name="arrow-forward"></ion-icon></span>',
-				centerMode: true,
-				centerPadding: '500px',
-				slidesToShow: 1,
-				dots: false,
-				speed: 900,
-				infinite: false,
-				cssEase: 'cubic-bezier(0.7, 0, 0.3, 1)',
-				touchThreshold: 100,
-				responsive: [
-					{
-						breakpoint: 1700,
-						settings: {
-							centerPadding: '400px',
-						}
-					},
-					{
-						breakpoint: 1440,
-						settings: {
-							centerPadding: '250px',
-						}
-					},
-				]
-			})
-		}
 	}
 
 	$( document ).on( 'ready', function () {
@@ -131,16 +98,34 @@
 
 		if ( $( '.about' ).length !== 0 ) {
 			let count = 0;
+			let i;
 
-			$( '.item h2' ).on( 'click', function () {
-				count += 1;
-				let $this = $( this );
+			const acc = document.querySelectorAll( '.item h2' );
+			const panels = document.querySelectorAll( '.item .content' );
 
-				$( '.item h2' ).removeClass( 'active' )
+			for ( i = 0; i < acc.length; i++ ) {
+				acc[i].addEventListener( 'click' , function() {
+					count += 1;
+					console.log(i)
 
-				count%2 == 0 ? $this.removeClass( 'active' ) : $this.addClass( 'active' )
-				$this.siblings( '.content' ).slideToggle( 300 )
-			} )
+					for (j = 0; j < panels.length; j++) {
+						if ( j !== i ) {
+							acc[j].classList.remove( 'active' );
+							panels[j].style.maxHeight = null;
+						}
+					}
+
+					this.classList.add( 'active' )
+
+					let panel = this.nextElementSibling;
+
+					if ( panel.style.maxHeight ) {
+						panel.style.maxHeight = null;
+					} else {
+						panel.style.maxHeight = panel.scrollHeight + "px";
+					}
+				});
+			}
 		}
 
 		if ( $( '.projects' ).length !== 0 ) {
